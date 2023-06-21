5f4ce17c43c2372d12d689a218e5279c5ef947c2
alan <matheusalan334@gmail.com>
2023-06-21 19:34:45 -0300

    commit atu

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const buts = document.querySelector('.btn1')
-
+const title = document.querySelector('.titulo')
 buts.addEventListener("click", function(){
     const main = document.querySelector('main')
     main.style.backgroundImage = "url('/projeto/imgs/desktop-wallpaper-rick-and-morty-throughout-the-incredible-rick-and-mor-papel-de-parede-pc-papel-de-parede-do-notebook-de-computador.jpg')"
-
+    title.style.color = 'white'
+    
 })
