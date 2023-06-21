acea8a453fcd522c118db63ba1bbff4a2c8a501a
alan <matheusalan334@gmail.com>
2023-06-20 21:23:36 -0300

    comitte atualizado 2

---
+++ script.js
@@ -0,0 +1,7 @@
+const buts = document.querySelector('.btn1')
+
+buts.addEventListener("click", function(){
+    const main = document.querySelector('main')
+    main.style.backgroundImage = "url('/projeto/imgs/desktop-wallpaper-rick-and-morty-throughout-the-incredible-rick-and-mor-papel-de-parede-pc-papel-de-parede-do-notebook-de-computador.jpg')"
+
+})
