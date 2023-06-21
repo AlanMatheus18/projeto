95ed002de81075b67fda2949bcc572cfc0e3f42b
alan <matheusalan334@gmail.com>
2023-06-21 20:35:37 -0300

    commit atua3

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const buts = document.querySelector('.btn1')
 const title = document.querySelector('.titulo')
+const rick = document.getElementById('rick')
 buts.addEventListener("click", function(){
     const main = document.querySelector('main')
     main.style.backgroundImage = "url('/projeto/imgs/desktop-wallpaper-rick-and-morty-throughout-the-incredible-rick-and-mor-papel-de-parede-pc-papel-de-parede-do-notebook-de-computador.jpg')"
     title.style.color = 'white'
-    
+    rick.src = 'none' 
+})
+
+const buts2 = document.querySelector('.btn2')
+
+buts2.addEventListener("click", function(){
+    const main2 = document.querySelector('main')
+    main2.style.backgroundImage = "none";
+    title.style.color = 'black'
+    rick.src = '/projeto/imgs/rick.png'
 })
